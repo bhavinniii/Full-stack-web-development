e1830617c26129243fcd174101e69e0f149af64b
bhavinniii <bhavini20.20@gmail.com>
2021-10-12 19:05:18 +0530

    path module

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
-
-const arr = [1,2,3,4,5]
-const val= JSON.stringify(arr)
-console.log(arr)
-console.log(val)
-
-localStorage.setItem("val", JSON.stringify(arr))
-console.log(JSON.parse(localStorage.getItem("val")))
+const { Console } = require("console")
+const path= require("path")
+console.log(path.resolve('./app.js'))
+console.log(path.relative('./',"./temp/utilis.js"))
+console.log(path.extname('./app.js'))
+console.log(path.dirname('./temp/utilis.js'))
+console.log(path.join(_dirname, 'app.js'))
