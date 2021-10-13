0b07128b0ba889cd07c6aac84554f79137446875
bhavinniii <bhavini20.20@gmail.com>
2021-10-14 01:10:06 +0530

    headers for authentication

--- app.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-fs.readFileSync('./test.txt', (error,data)=>{
-    if(error) console.log(error)
-else console.log(typeof data)
-})
-console.log(fs.readFileSync('./test.txt', 'utf-8'))
- 
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const PORT = 3000
+const app = express()
+
+const isAdmin = (req, res, next) => {
+    if (req.headers.admin === 'true') next()
+    else res.send("UNAUTHORISED")
+}
+
+
+app.get('/public', (req, res) => {
+    console.log(req.headers)
+    res.send(`I'm a public route`)
+})
+
+app.get('/private', isAdmin, (req, res) => {
+    res.send(`I'm a admin route`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
+})
